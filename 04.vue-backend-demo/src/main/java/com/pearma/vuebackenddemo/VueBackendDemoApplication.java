cccf39a716d8241d96e734b8ee7bff2dc265a889
Victor Ma <pearma@gmail.com>
2021-10-31 10:21:38 +0800

    新增demo

---
+++ 04.vue-backend-demo/src/main/java/com/pearma/vuebackenddemo/VueBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pearma.vuebackenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueBackendDemoApplication.class, args);
+	}
+
+}
---
+++ 04.vue-backend-demo/src/test/java/com/pearma/vuebackenddemo/VueBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pearma.vuebackenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
